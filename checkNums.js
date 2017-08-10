c105a4b91eede9da327b763993e79068a10115ec
shakira91 <shakira91@users.noreply.github.com>
2017-08-10 15:13:06 -0400

    Update checkNums.js

--- checkNums.js
+++ checkNums.js
@@ -1,3 +1,10 @@
+// Have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false. If the parameter values are equal to each other then return the string -1. 
+
+// Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+
+
+
 function CheckNums(num1,num2) { 
 
 if (num2 > num1) {
