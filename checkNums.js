51298441d5c6e42f41b6d4e6ce114fefa27f97d5
shakira91 <shakira91@users.noreply.github.com>
2017-08-09 22:57:11 -0400

    second challenge

---
+++ checkNums.js
@@ -0,0 +1,20 @@
+function CheckNums(num1,num2) { 
+
+if (num2 > num1) {
+   
+    return 'true'; 
+    
+}
+
+else if (num2 === num1) {
+    return -1;
+}
+
+else {
+    return 'false';
+}// code goes here  
+         
+}
+   
+// keep this function call here 
+CheckNums(readline());
